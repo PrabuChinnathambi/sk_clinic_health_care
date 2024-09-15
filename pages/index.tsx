be915e5e311fd135c9862eaacb0ac0a957b99008
prabudev-00 <prabu.c@thinkaside.com>
2024-09-15 10:59:21 +0530

    build issue fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ export default function Home() {
         <title>SK Heralthcare</title>
         <meta name="description" content="Healthcare Clinic" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/public/assets/images/logo.jpg" />
+        <link rel="icon" href="../public/assets/images/logo.jpg" />
       </Head>
       <main className="w-full h-screen font-bodyFont bg-bodyColor text-textLight  overflow-x-hidden overflow-y-scroll scrollbar scrollbar-track-textDark/20 scrollbar-thumb-textDark/60">
         <Navbar />
