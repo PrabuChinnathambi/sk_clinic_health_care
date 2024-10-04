6c27a1446054485c9f52b5445498fc5331cd7043
Sivaprasath0371 <sivaprasathuiuxdesigner>
2024-10-04 20:48:21 +0530

    social media link added

--- components/LeftSide.tsx
+++ components/LeftSide.tsx
@@ -20,6 +20,11 @@ const LeftSide = () => {
   const phoneNumber = "+917598776007"; // Replace with your WhatsApp number (including country code)
   const message = "Hello, I’d like to inquire about your service."; // Pre-filled message
 
+  const email = "skhealthcare0709@gmail.com";
+
+  const subject = "Hello!";
+  const body = "I hope this message finds you well.";
+
   return (
     <div className="w-full h-full flex flex-col items-center justify-end gap-4 text-textLight">
       <div className="flex flex-col gap-4">
@@ -34,7 +39,7 @@ const LeftSide = () => {
           </span>
         </a>
         <a
-          href="https://www.youtube.com/channel/UChkOsij0dhgft0GhHRauOAA"
+          href="https://www.facebook.com/share/YZkk5KJHmcEzMSdZ/?mibextid=qi2Omg"
           target="_blank"
         >
           <span className="w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300">
@@ -42,19 +47,22 @@ const LeftSide = () => {
           </span>
         </a>
         <a
-          href="https://www.linkedin.com/in/noor-mohammad-ab2245193/"
+          href="https://www.instagram.com/skhealthcare0709/?igsh=Y3EwemdoNXpmb2dv"
           target="_blank"
         >
           <span className="w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300">
             <SlSocialInstagram />
           </span>
         </a>
-        <a href="https://www.facebook.com/Noorlalu143/" target="_blank">
+        <a href={`mailto:${email}`} target="_blank">
           <span className="w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300">
             <RiMailSettingsLine />
           </span>
         </a>
-        <a href="https://www.instagram.com/simplenoor143/" target="_blank">
+        <a
+          href="https://www.linkedin.com/in/sk-healthcare-495060331/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+          target="_blank"
+        >
           <span className="w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300">
             {/* <SlSocialInstagram /> */}
             <FaLinkedin />
